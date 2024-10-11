78bb3dfbfa8c1cc487a05caa2b082b11cc6218f4
Bender <public@mail.ru>
2024-10-11 13:06:40 +0300

    add Package Constants and Class Alphabets.

---
+++ src/main/java/ru/javarush/bichahchyan/caesarcipher/Constans/Alphabets.java
@@ -0,0 +1,16 @@
+package ru.javarush.bichahchyan.caesarcipher.Constans;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Alphabets {
+    public static final List<Character> RUS_ALPHABET = Arrays.asList('а', 'б',
+            'в','г', 'д', 'е', 'ж', 'з', 'и', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у',
+            'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'я', '.', ',', '«', '»',
+            ':', '!', '?', ' ');
+
+    public static final List<Character> ENG_ALPHABET = Arrays.asList('a', 'b',
+            'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o',
+            'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '.', ',', '«', '»',
+            ':', '!', '?', ' ');
+}
