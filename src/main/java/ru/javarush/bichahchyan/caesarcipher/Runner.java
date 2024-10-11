203b28b28b9cf57ff1092f334990d4e9335c6d5d
Bender <public@mail.ru>
2024-10-11 14:15:06 +0300

    added main logical cod for  class Runner

--- src/main/java/ru/javarush/bichahchyan/caesarcipher/Runner.java
+++ src/main/java/ru/javarush/bichahchyan/caesarcipher/Runner.java
@@ -1,7 +1,37 @@
 package ru.javarush.bichahchyan.caesarcipher;
 
+import ru.javarush.bichahchyan.caesarcipher.Tools.ConsoleManager;
+
 public class Runner {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        System.out.println("Добро пожаловать в меню Шифра Цезоря");
+        boolean isStopped = false;
+        while(!isStopped){
+            Commander.showMenu();
+            String commandNumber = ConsoleManager.getKeyForMenu();
+            if(commandNumber != null) {
+                switch (commandNumber) {
+                    case "1":
+                        System.out.println("Для шифрования укажите путь к файлу , место куда сохранить результат и ключ");
+                        Commander.encryptCommand();
+                        break;
+                    case "2":
+                        System.out.println("Для Расшифровки укажите путь к зашифрованному файлу , место куда сохранить результат и ключ");
+                        Commander.decryptCommand();
+                        break;
+                    case "3":
+                        System.out.println("Для команды BruteForce нужно указать путь к файлу");
+                        Commander.brutForceCommand();
+                        break;
+                    case "exit":
+                        isStopped = true;
+                        System.out.println("Выход из приложения");
+                        break;
+                    default:
+                        System.out.println("Unknown Command");
+                        break;
+                }
+            }
+        }
     }
 }
